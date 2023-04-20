ed4cd3ad221b91c9009b464748d570e539608f96
Artur Vieira <artur.vieira@frax.com>
2023-04-20 13:36:08 +0200

    Replace sensitive data with .env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyAM5BTTHMuVfOFXJcr28TiInL4kgt6j9yc',
-	authDomain: 'work-days-32676.firebaseapp.com',
-	projectId: 'work-days-32676',
-	storageBucket: 'work-days-32676.appspot.com',
-	messagingSenderId: '25856752246',
-	appId: '1:25856752246:web:b1b591eb5b595c449bfd57',
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
