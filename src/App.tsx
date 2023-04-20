b8769255ce84ec33059a6581529b352a94ad48e7
Artur Vieira <artur.soareshsv@gmail.com>
2023-04-20 13:39:34 +0200

    Firebase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { Card, CardContent, Grid, Stack, Typography } from '@mui/material';
 import eachDayOfInterval from 'date-fns/eachDayOfInterval';
 import endOfMonth from 'date-fns/endOfMonth';
+import format from 'date-fns/format';
 import isWeekend from 'date-fns/isWeekend';
 import startOfMonth from 'date-fns/startOfMonth';
 import { Formik } from 'formik';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { AutoSubmit } from './components/AutoSubmit';
 import { FormikDatePicker } from './components/FormikDatePicker';
 import { FormikNumberField } from './components/FormikNumberField';
+import { getOfficeDays } from './services/officeDays';
 
 type FormData = {
 	from: Date;
@@ -17,51 +19,71 @@ type FormData = {
 
 const DATE_FORMAT = 'dd/MM/yyyy';
 
-function App() {
+export default function App() {
 	const initialValues: FormData = {
 		from: startOfMonth(new Date()),
 		to: endOfMonth(new Date()),
-		holidays: 3,
+		holidays: 0,
 	};
 
 	const [workDays, setWorkDays] = useState<number>(0);
+	const [officeDays, setOfficeDays] = useState<Date[]>();
 
-	const handleSubmit = ({ from, to, holidays }: FormData): void => {
-		const days = eachDayOfInterval({ start: from, end: to });
-		const weekDays = days.filter((day) => !isWeekend(day));
+	useEffect(() => {
+		getOfficeDays().then((dates) => setOfficeDays(dates));
+	}, []);
 
-		setWorkDays(weekDays.length - holidays);
-	};
+	const handleSubmit = useCallback(
+		({ from, to, holidays }: FormData): void => {
+			const days = eachDayOfInterval({ start: from, end: to });
+			const weekDays = days.filter((day) => !isWeekend(day));
+
+			setWorkDays(weekDays.length - holidays - (officeDays?.length ?? 0));
+		},
+		[officeDays]
+	);
 
 	return (
 		<Card variant='outlined'>
 			<CardContent>
-				<Typography variant='h5' component='div'>
-					Work days
-				</Typography>
+				<Stack spacing={2}>
+					<Typography variant='h5' component='div' gutterBottom>
+						Work days
+					</Typography>
 
-				<Formik initialValues={initialValues} onSubmit={handleSubmit}>
-					<Grid container spacing={2}>
-						<Grid item>
-							<FormikDatePicker label='From' name='from' format={DATE_FORMAT} />
+					<Formik initialValues={initialValues} onSubmit={handleSubmit}>
+						<Grid container gap={2}>
+							<Grid item>
+								<FormikDatePicker label='From' name='from' format={DATE_FORMAT} />
+							</Grid>
+
+							<Grid item>
+								<FormikDatePicker label='To' name='to' format={DATE_FORMAT} />
+							</Grid>
+
+							<Grid item>
+								<FormikNumberField label='Holidays' name='holidays' />
+							</Grid>
+
+							<AutoSubmit dependencies={[officeDays]} />
 						</Grid>
+					</Formik>
 
-						<Grid item>
-							<FormikDatePicker label='To' name='to' format={DATE_FORMAT} />
-						</Grid>
+					<Stack gap={0.5}>
+						<Typography variant='subtitle1'>
+							<strong>Business days:</strong>
+						</Typography>
+						<Typography>- {workDays}</Typography>
 
-						<Grid item>
-							<FormikNumberField label='Holidays' name='holidays' />
-						</Grid>
-
-						<AutoSubmit />
-					</Grid>
-				</Formik>
-
-				<Typography>{workDays}</Typography>
+						<Typography variant='subtitle1'>
+							<strong>Office days:</strong>
+						</Typography>
+						{officeDays?.map((officeDay) => (
+							<Typography key={officeDay.toISOString()}>- {format(officeDay, DATE_FORMAT)}</Typography>
+						))}
+					</Stack>
+				</Stack>
 			</CardContent>
 		</Card>
 	);
 }
-
-export default App;
--- src/components/AutoSubmit.tsx
+++ src/components/AutoSubmit.tsx
@@ -1,12 +1,16 @@
 import { useFormikContext } from 'formik';
 import { useEffect } from 'react';
 
-export function AutoSubmit() {
+type Props = {
+	dependencies?: any[];
+};
+
+export function AutoSubmit({ dependencies }: Props) {
 	const { values, submitForm } = useFormikContext();
 
 	useEffect(() => {
 		submitForm();
-	}, [values, submitForm]);
+	}, [values, submitForm, dependencies]);
 
 	return null;
 }
